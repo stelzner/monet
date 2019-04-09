Add Clevr experiment and fix KL-div

datasets.py
@@ -5,6 +5,7 @@ import torch
 from torch.utils.data import Dataset
 import numpy as np
 from numpy.random import random_integers
+from PIL import Image
 
 
 def progress_bar(count, total, status=''):
@@ -53,8 +54,6 @@ def make_sprites(n=50000, height=64, width=64):
             images[i] += sprite
         if i % 100 == 0:
             progress_bar(i, n)
-            # print('{}/{}'.format(i, n))
-    # print('{}/{}'.format(n, n))
     images = np.clip(images, 0.0, 1.0)
 
     return {'x_train': images[:4 * n // 5],
@@ -75,7 +74,6 @@ class Sprites(Dataset):
 
         self.transform = transform
         self.images = data['x_train'] if train else data['x_test']
-        # self.images = np.transpose(self.images, (0, 3, 1, 2))
         self.counts = data['count_train'] if train else data['count_test']
 
     def __len__(self):
@@ -86,4 +84,22 @@ class Sprites(Dataset):
         if self.transform is not None:
             img = self.transform(img)
         return img, self.counts[idx]
+
+
+class Clevr(Dataset):
+    def __init__(self, directory, transform=None):
+        self.directory = directory
+        self.filenames = os.listdir(directory)
+        self.n = len(self.filenames)
+        self.transform = transform
+
+    def __len__(self):
+        return self.n
+
+    def __getitem__(self, idx):
+        imgpath = os.path.join(self.directory, self.filenames[idx])
+        img = Image.open(imgpath)
+        if self.transform is not None:
+            img = self.transform(img)
+        return img, 1
 

main.py
@@ -6,6 +6,8 @@ import torch.optim as optim
 import numpy as np
 import visdom
 
+import os
+
 import model
 import datasets
 
@@ -14,8 +16,13 @@ vis = visdom.Visdom()
 class MonetArgs:
     def __init__(self):
         self.vis_every = 50
+        self.num_slots = 4
         self.load_parameters = True
         self.checkpoint_file = './checkpoints/monet.ckpt'
+        self.batch_size = 64
+        self.num_epochs = 20
+        self.num_blocks = 5
+        self.channel_base = 64
 
 def numpify(tensor):
     return tensor.cpu().detach().numpy()
@@ -35,31 +42,19 @@ def visualize_masks(imgs, masks, recons):
     seg_maps /= 255.0
     vis.images(np.concatenate((imgs, seg_maps, recons), 0), nrow=imgs.shape[0])
 
-
-if __name__ == '__main__':
-    args = MonetArgs()
-    batch_size = 64
-    transform = transforms.Compose([transforms.ToTensor(),
-                                    transforms.Lambda(lambda x: x.float()),
-                                    ])
-    trainset = datasets.Sprites(train=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                              shuffle=True, num_workers=2)
-    monet = model.Monet().cuda()
-
-    if args.load_parameters:
+def run_training(monet, args, trainloader):
+    if args.load_parameters and os.path.isfile(args.checkpoint_file):
         monet.load_state_dict(torch.load(args.checkpoint_file))
+        print('Restored parameters from', args.checkpoint_file)
     else:
         for w in monet.parameters():
-            # print(w.shape)
-            # print('before', w.min(), w.max())
             std_init = 0.01
             nn.init.normal_(w, mean=0., std=std_init)
-            # print('after', w.min(), w.max())
+        print('Initialized parameters')
 
     optimizer = optim.RMSprop(monet.parameters(), lr=1e-4)
 
-    for epoch in range(20):
+    for epoch in range(args.num_epochs):
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
             images, counts = data
@@ -81,4 +76,46 @@ if __name__ == '__main__':
 
         torch.save(monet.state_dict(), args.checkpoint_file)
 
-    print('done')
+    print('training done')
+
+def sprite_experiment():
+    args = MonetArgs()
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Lambda(lambda x: x.float()),
+                                    ])
+    trainset = datasets.Sprites(train=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset,
+                                              batch_size=args.batch_size,
+                                              shuffle=True, num_workers=2)
+    monet = model.Monet(args, 64, 64).cuda()
+    run_training(monet, args, trainloader)
+
+def clevr_experiment():
+    args = MonetArgs()
+    args.channel_base = 16
+    args.batch_size = 20
+    args.num_slots = 11
+    args.num_blocks = 6
+    args.checkpoint_file = './checkpoints/clevr.ckpt'
+    # Crop as described in appendix C
+    crop_tf = transforms.Lambda(lambda x: transforms.functional.crop(x, 29, 64, 192, 192))
+    drop_alpha_tf = transforms.Lambda(lambda x: x[:3])
+    transform = transforms.Compose([crop_tf,
+                                    transforms.Resize((128, 128)),
+                                    transforms.ToTensor(),
+                                    drop_alpha_tf,
+                                    transforms.Lambda(lambda x: x.float()),
+                                   ])
+    trainset = datasets.Clevr('/data/stelzner/data/CLEVR_v1.0/images/train',
+                              transform=transform)
+
+    trainloader = torch.utils.data.DataLoader(trainset,
+                                              batch_size=args.batch_size,
+                                              shuffle=True, num_workers=4)
+    monet = model.Monet(args, 128, 128).cuda()
+    run_training(monet, args, trainloader)
+
+if __name__ == '__main__':
+    clevr_experiment()
+    # sprite_experiment()
+

model.py
@@ -59,22 +59,26 @@ class UNet(nn.Module):
 
 
 class AttentionNet(nn.Module):
-    def __init__(self):
+    def __init__(self, args):
         super().__init__()
-        self.unet = UNet(num_blocks=5, in_channels=4, out_channels=2)
+        self.args = args
+        self.unet = UNet(num_blocks=args.num_blocks,
+                         in_channels=4,
+                         out_channels=2,
+                         channel_base=args.channel_base)
 
     def forward(self, x, scope):
         inp = torch.cat((x, scope), 1)
         logits = self.unet(inp)
-        log_alpha = torch.softmax(logits, 1)
-        # output channel 0 represents log alpha_k,
-        # channel 1 represents log (1 - alpha_k).
-        mask = scope * log_alpha[:, 0:1]
-        new_scope = scope * log_alpha[:, 1:2]
+        alpha = torch.softmax(logits, 1)
+        # output channel 0 represents alpha_k,
+        # channel 1 represents (1 - alpha_k).
+        mask = scope * alpha[:, 0:1]
+        new_scope = scope * alpha[:, 1:2]
         return mask, new_scope
 
 class EncoderNet(nn.Module):
-    def __init__(self):
+    def __init__(self, width, height):
         super().__init__()
         self.convs = nn.Sequential(
             nn.Conv2d(4, 32, 3, stride=2),
@@ -86,8 +90,11 @@ class EncoderNet(nn.Module):
             nn.Conv2d(64, 64, 3, stride=2),
             nn.ReLU(inplace=True)
         )
+        for i in range(4):
+            width = (width - 1) // 2
+            height = (height - 1) // 2
         self.mlp = nn.Sequential(
-            nn.Linear(64 * 3 * 3, 256),
+            nn.Linear(64 * width * height, 256),
             nn.ReLU(inplace=True),
             nn.Linear(256, 32)
         )
@@ -99,10 +106,10 @@ class EncoderNet(nn.Module):
         return x
 
 class DecoderNet(nn.Module):
-    def __init__(self, img_width=64, img_height=64):
+    def __init__(self, height, width):
         super().__init__()
-        self.img_width = img_width
-        self.img_height = img_height
+        self.height = height
+        self.width = width
         self.convs = nn.Sequential(
             nn.Conv2d(18, 32, 3),
             nn.ReLU(inplace=True),
@@ -113,16 +120,15 @@ class DecoderNet(nn.Module):
             nn.Conv2d(32, 32, 3),
             nn.ReLU(inplace=True),
             nn.Conv2d(32, 4, 1),
-            nn.Sigmoid()
         )
-        ys = torch.linspace(-1, 1, self.img_height+8)
-        xs = torch.linspace(-1, 1, self.img_width+8)
+        ys = torch.linspace(-1, 1, self.height + 8)
+        xs = torch.linspace(-1, 1, self.width + 8)
         ys, xs = torch.meshgrid(ys, xs)
-        self.coord_map = torch.stack((ys, xs)).unsqueeze(0)
-        self.register_buffer('coord_map_const', self.coord_map)
+        coord_map = torch.stack((ys, xs)).unsqueeze(0)
+        self.register_buffer('coord_map_const', coord_map)
 
     def forward(self, z):
-        z_tiled = z.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, self.img_height+8, self.img_width+8)
+        z_tiled = z.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, self.height + 8, self.width + 8)
         coord_map = self.coord_map_const.repeat(z.shape[0], 1, 1, 1)
         inp = torch.cat((z_tiled, coord_map), 1)
         result = self.convs(inp)
@@ -130,12 +136,12 @@ class DecoderNet(nn.Module):
 
 
 class Monet(nn.Module):
-    def __init__(self):
+    def __init__(self, args, height, width):
         super().__init__()
-        self.attention = AttentionNet()
-        self.encoder = EncoderNet()
-        self.decoder = DecoderNet()
-        self.k = 4
+        self.args = args
+        self.attention = AttentionNet(args)
+        self.encoder = EncoderNet(height, width)
+        self.decoder = DecoderNet(height, width)
         self.beta = 0.5
         self.gamma = 0.25
         self.masks = None
@@ -144,31 +150,36 @@ class Monet(nn.Module):
     def forward(self, x):
         scope = torch.ones_like(x[:, 0:1])
         masks = []
-        for i in range(self.k-1):
+        for i in range(self.args.num_slots-1):
             mask, scope = self.attention(x, scope)
             masks.append(mask)
         masks.append(scope)
         loss = torch.zeros_like(x[:, 0, 0, 0])
         mask_preds = []
         complete_recon = torch.zeros_like(x)
+        p_xs = torch.zeros_like(loss)
+        kl_zs = torch.zeros_like(loss)
         for i, mask in enumerate(masks):
             z, kl_z = self.__encoder_step(x, mask)
             sigma = 0.09 if i == 0 else 0.11
             p_x, x_recon, mask_pred = self.__decoder_step(x, z, mask, sigma)
             mask_preds.append(mask_pred)
-            # print('px', p_x.mean().item())
-            # print('klz', kl_z.mean().item())
             loss += -p_x + self.beta * kl_z
+            p_xs += -p_x
+            kl_zs += kl_z
             complete_recon += mask * x_recon
 
         self.masks = torch.cat(masks, 1)
         self.complete_recon = complete_recon
         masks = torch.transpose(self.masks, 1, 3)
-        q_masks = dists.Categorical(logits=masks)
+        # print(masks[0, 0:3, :3])
+        q_masks = dists.Categorical(probs=masks)
         q_masks_recon = dists.Categorical(logits=torch.stack(mask_preds, 3))
-        kl_masks = dists.kl_divergence(q_masks_recon, q_masks)
+        kl_masks = dists.kl_divergence(q_masks, q_masks_recon)
         kl_masks = torch.sum(kl_masks, [1, 2])
-        # print('kl masks', kl_masks.mean().item())
+        print('px', p_xs.mean().item(),
+              'kl_z', kl_zs.mean().item(),
+              'kl masks', kl_masks.mean().item())
         loss += self.gamma * kl_masks
         return loss
 
@@ -181,24 +192,17 @@ class Monet(nn.Module):
         dist = dists.Normal(means, sigmas)
         dist_0 = dists.Normal(0., sigmas)
         z = means + dist_0.sample()
-        # print('z', z.min().item(), z.max().item())
         q_z = dist.log_prob(z)
-        # print('means', means)
         kl_z = dists.kl_divergence(dist, dists.Normal(0., 1.))
-        # print('px dists', dist)
-        # print('kl_z', kl_z)
         kl_z = torch.sum(kl_z, 1)
         return z, kl_z
 
     def __decoder_step(self, x, z, mask, sigma):
         decoder_output = self.decoder(z)
-        x_recon = decoder_output[:, :3]
-        # print('recon', x_recon.min().item(), x_recon.max().item())
+        x_recon = torch.sigmoid(decoder_output[:, :3])
         mask_pred = decoder_output[:, 3]
         dist = dists.Normal(x_recon, sigma)
         p_x = dist.log_prob(x)
-        # print('px min/max', p_x.min().item(), p_x.max().item())
-        # print(mask.shape, p_x.shape)
         p_x *= mask
         p_x = torch.sum(p_x, [1, 2, 3])
         return p_x, x_recon, mask_pred